d6995f085488d4ff493aff4fd4efe4fa039a18a6
Nolen Johnson <johnsonnolen@gmail.com>
2026-03-17 13:14:47 -0400

    Initial Extraction

---
+++ Android.mk
@@ -0,0 +1,29 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),pyrite)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,8705b53dc0a3a84ef480c701211992764b03d7cb)
+$(call add-radio-file-sha1-checked,radio/aop.img,15798209448c34a46a6841f24198dd0ba7c62f7f)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,3dd7190814c960bdf637ad65b6fe9e38560c3411)
+$(call add-radio-file-sha1-checked,radio/cpucp.img,9f74b98bafc7a99b8f77883de14b81f72f28644b)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,30f85e4b7d170484966a78dd828e3f4ce52e131b)
+$(call add-radio-file-sha1-checked,radio/dsp.img,fd77154c018a3a83ec827ffabcb3cfa1714408c1)
+$(call add-radio-file-sha1-checked,radio/featenabler.img,9c83861076b37d62e9fc30595db8aac3af071060)
+$(call add-radio-file-sha1-checked,radio/hyp.img,5d2c389e62379a4b903df8561dbd4e563d0ee14e)
+$(call add-radio-file-sha1-checked,radio/imagefv.img,7d76d48d64d7ac5411d714a4bb83f37e3e5b8df6)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,83534b7a36118ce1ac1e3c4621459d2ad48e76d2)
+$(call add-radio-file-sha1-checked,radio/modem.img,2987eed21974719595a66443ad67bb7e2ea27074)
+$(call add-radio-file-sha1-checked,radio/multiimgoem.img,214c0cd90d97f69dbe47b538e681c393bfd17694)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,ebfc7c889c78071a5f0ee08accbfcf5b96a5a506)
+$(call add-radio-file-sha1-checked,radio/qweslicstore.img,180cf2903b9872499d05eeef08c472575fa3225e)
+$(call add-radio-file-sha1-checked,radio/shrm.img,939ebffbffaacc28af30265131100bf41fc9a78e)
+$(call add-radio-file-sha1-checked,radio/tz.img,53cf6f57fe2645464854aee349f5ae6f53bdf32e)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,79f474087eafd046e67bee0e24be9e4ca4d77b8c)
+$(call add-radio-file-sha1-checked,radio/xbl.img,13c6553e81758e18dfa211dff46eab0e5e253564)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,a018ee8538700977441206f3a4c05a93ab4f9693)
+
+endif
